d42c3b77370a7f105694493f10c52bbe3a12f438
unknown <whernandez@grupogd.com.sv>
2022-06-12 13:49:13 -0600

    Footer and Menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ca_ES } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ca from '@angular/common/locales/ca';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(ca);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SharedModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    MenuComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    MenuComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
